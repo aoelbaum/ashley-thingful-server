959a23be702241d5fafcb5d0b81efd29039b9777
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-31 12:57:18 -0400

    incorporated jwt and auth-router, auth-router tests passing

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const AuthService = require('../Auth/auth-service');
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || '';
   let basicToken;
@@ -8,23 +8,22 @@ function requireAuth(req, res, next) {
   else {
     basicToken = authToken.slice('basic '.length, authToken.length);
   }
-  const [tokenUserName, tokenPassword] = Buffer
-    .from(basicToken, 'base64')
-    .toString()
-    .split(':');
-  
+  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
   if (!tokenUserName || !tokenPassword) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
+
+  AuthService.getUserWithUserName(
+    req.app.get('db'),
+    tokenUserName
+  )
   
-  req.app.get('db')('thingful_users')
-    .where({ user_name: tokenUserName })
-    .first()
+ 
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: 'Unauthorized request' });
       }
-      return bcrypt.compare(tokenPassword, user.password)
+      return AuthService.comparePasswords(tokenPassword, user.password)
         .then(passwordsMatch => {
           if (!passwordsMatch) {
             return res.status(401).json({ error: 'Unauthorized request' });
